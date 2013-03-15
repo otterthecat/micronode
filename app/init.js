d181b65e43d874974cc539fcde235e057247b9e1
Otter <otc@otterthecat.com>
2013-03-14 22:35:11 -0500

    removed unused files

--- app/init.js
+++
@@ -1,6 +0,0 @@
-var appServer = require('./server/server.js');
-var router = require('./server/router.js');
-
-// passing port number and routing object
-// init the server
-appServer.startUp(8080, router);
