0de82f5f21c454ab3e9faa250d25a215e0aab250
Otter <otc@otterthecat.com>
2013-03-19 22:34:29 -0500

    Router.js is starting to do actual routing

--- app/server/router.js
+++ app/server/router.js
@@ -6,4 +6,29 @@ var headers = {
 	ico: {type: '', basePath: '../../img'}
 };
 
+var routes = {};
+
+var set = function(request_path, cb, func, request_data){
+
+	routes[request_path] = {
+		data: request_data || null,
+		callback: func || null
+	};
+
+	return cb();
+};
+
+var get = function(path_str, callback){
+
+	if(!path_str || typeof path_str !== 'string'){
+
+		return callback(routes);
+	} else if(routes.hasOwnProperty(path_str)) {
+
+		return callback(routes[path_str]);
+	}
+};
+
 exports.headers = headers;
+exports.get 	= get;
+exports.set 	= set;
--- app/server/server.js
+++ app/server/server.js
@@ -2,24 +2,55 @@ var http 		= require('http');
 var url  		= require('url');
 var fs 			= require('fs');
 var path 		= require('path');
-var headers 	= require('./router').headers;
+var router 		= require('./router');
 
+var headers = router.headers
+
+
+router.set('/my/path.html', function(){
+
+	console.log('setting my path');
+});
+
+router.set('/testing-route.html', function(){
+
+	console.log('setting testing route');
+}, "fake string", function(){});
+
+
+var r = router.get('/testing-route', function(data){
+
+	console.log('getting router : ');
+	console.log(data);
+	return data;
+});
 
 function onRequest(req, res){
 
 	var called_file = url.parse(req.url).pathname;
+	var file_ext = path.extname(called_file).substr(1);
+	console.log(called_file);
+	console.log('file ext = ' + file_ext);
+
 	called_file = (called_file === '/') ? '/index.html' : called_file;
 
-	var file_ext = path.extname(called_file).substr(1);
+	if(file_ext === 'html'){
 
-	console.log(called_file);
+		router.get(called_file, function(data){
 
-	fs.readFile(headers[file_ext].basePath + called_file, function(err, data){
+			console.log('router is calling stuff');
+			console.log(data);
+		});
 
-		res.writeHeader(200, {'Content-Type': headers[file_ext].type});
-		res.write(data);
-		res.end();
-	});
+	} else {
+
+		fs.readFile(headers[file_ext].basePath + called_file, function(err, data){
+
+			res.writeHeader(200, {'Content-Type': headers[file_ext].type});
+			res.write(data);
+			res.end();
+		});
+	}
 };
 
 var server = http.createServer(onRequest).listen(1337);
