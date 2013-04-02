e8f78ef29678731c8296e41cff79028e0e30628b
Otter <otc@otterthecat.com>
2013-04-01 21:49:34 -0500

    Fixing the server/routing system

--- app/server/server.js
+++ app/server/server.js
@@ -28,12 +28,10 @@ var r = router.get('/testing-route', function(data){
 function onRequest(req, res){
 
 	var called_file = url.parse(req.url).pathname;
-	var file_ext = path.extname(called_file).substr(1);
-	console.log(called_file);
-	console.log('file ext = ' + file_ext);
-
 	called_file = (called_file === '/') ? '/index.html' : called_file;
 
+	var file_ext = path.extname(called_file).substr(1);
+/*
 	if(file_ext === 'html'){
 
 		router.get(called_file, function(data){
@@ -43,14 +41,15 @@ function onRequest(req, res){
 		});
 
 	} else {
+*/
 
-		fs.readFile(headers[file_ext].basePath + called_file, function(err, data){
+	fs.readFile(headers[file_ext].basePath + called_file, function(err, data){
 
-			res.writeHeader(200, {'Content-Type': headers[file_ext].type});
-			res.write(data);
-			res.end();
-		});
-	}
+		res.writeHeader(200, {'Content-Type': headers[file_ext].type});
+		res.write(data);
+		res.end();
+	});
+//	}
 };
 
 var server = http.createServer(onRequest).listen(1337);
