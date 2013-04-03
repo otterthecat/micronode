88494ce9ef5bb82815090f1e265b38391a3e317e
Otter <otc@otterthecat.com>
2013-04-02 21:06:09 -0500

    Refactored router.js and upated server.js accordingly

--- app/server/router.js
+++ app/server/router.js
@@ -1,34 +1,65 @@
+var url  		= require('url');
+var fs 			= require('fs');
+var path 		= require('path');
+
+
 var headers = {
 
 	html: {type: 'text/html', basePath: '../../templates'},
 	js: {type: 'text/javascript', basePath: '../../js'},
 	css: {type: 'text/css', basePath: '../..'},
-	ico: {type: '', basePath: '../../img'}
+	ico: {type: '', basePath: '../../img'},
+	png: {type: 'image/png', basePath: '../../img'},
+	jpg: {type: 'image/jpeg', basePath: '../../img'}
 };
 
-var routes = {};
+var routes = function(){
 
-var set = function(request_path, cb, func, request_data){
+	this.data  = [];
+	this.template = "/index.html";
+	this.callback = function(){};
 
-	routes[request_path] = {
-		data: request_data || null,
-		callback: func || null
+	return this;
+};
+
+var applyRoute = function(prop_obj){
+
+	var r = new routes();
+
+	for(item in prop_obj){
+
+		r[item] = prop_obj.item;
 	};
 
-	return cb();
+	return r;
 };
 
-var get = function(path_str, callback){
+var set = function(request_path, param_obj, callback){
 
-	if(!path_str || typeof path_str !== 'string'){
+	routes[request_path] = applyRoute(param_obj);
 
-		return callback(routes);
-	} else if(routes.hasOwnProperty(path_str)) {
-
-		return callback(routes[path_str]);
-	}
+	return callback();
 };
 
-exports.headers = headers;
-exports.get 	= get;
+var route = function(request, response, callback){
+
+	var called_file = url.parse(request.url).pathname;
+	called_file = (called_file === '/') ? '/index.html' : called_file;
+
+	var file_ext = path.extname(called_file).substr(1);
+
+	var requested_file = routes[called_file] != undefined ? routes[called_file].template : called_file;
+
+	fs.readFile(headers[file_ext].basePath + requested_file, function(err, data){
+
+		response.writeHeader(200, {'Content-Type': headers[file_ext].type});
+		response.write(data);
+		response.end();
+
+		callback(data);
+	});
+};
+
+
+exports.route 	= route;
 exports.set 	= set;
--- app/server/server.js
+++ app/server/server.js
@@ -1,55 +1,30 @@
 var http 		= require('http');
-var url  		= require('url');
-var fs 			= require('fs');
-var path 		= require('path');
 var router 		= require('./router');
 
-var headers = router.headers
 
-
-router.set('/my/path.html', function(){
+router.set('/my/path.html', {}, function(){
 
 	console.log('setting my path');
 });
 
-router.set('/testing-route.html', function(){
+router.set('/', {}, function(){
+
+	console.log("setting index");
+});
+
+router.set('/testing-route.html', {}, function(){
 
 	console.log('setting testing route');
-}, "fake string", function(){});
-
-
-var r = router.get('/testing-route', function(data){
-
-	console.log('getting router : ');
-	console.log(data);
-	return data;
 });
+
 
 function onRequest(req, res){
 
-	var called_file = url.parse(req.url).pathname;
-	called_file = (called_file === '/') ? '/index.html' : called_file;
 
-	var file_ext = path.extname(called_file).substr(1);
-/*
-	if(file_ext === 'html'){
+	router.route(req, res, function(resp){
 
-		router.get(called_file, function(data){
-
-			console.log('router is calling stuff');
-			console.log(data);
-		});
-
-	} else {
-*/
-
-	fs.readFile(headers[file_ext].basePath + called_file, function(err, data){
-
-		res.writeHeader(200, {'Content-Type': headers[file_ext].type});
-		res.write(data);
-		res.end();
+		console.log('request has been routed');;
 	});
-//	}
 };
 
 var server = http.createServer(onRequest).listen(1337);
