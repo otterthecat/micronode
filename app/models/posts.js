438c6f2ea03d04886edce63bca8078dfb1d29d89
Otter <otc@otterthecat.com>
2013-06-30 00:47:47 -0500

    Fixed model name

---
+++ app/models/posts.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = mongoose.Types.ObjectId
+
+mongoose.connect("mongodb://localhost/test");
+
+var Post_Schema = new Schema({
+
+	title: {type: String},
+	summary: {type: String}
+	post: {type: String},
+	date: {type: Date, default: Date.now},
+	tags: {type: [String]}
+});
+
+var Posts = mongoose.model('Posts', Post_Schema);
+
+exports.mongoose = mongoose;
+exports.schema = Schema;
+exports.model = Posts;
+exports.objectId = ObjectId;
